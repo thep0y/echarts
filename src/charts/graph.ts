28140e714f24ee95ce912f6d6076aced35988b27
thepoy <thepoy@163.com>
2023-05-15 15:39:53 +0800

    初步完成联合发文机关关系图

--- src/charts/graph.ts
+++ src/charts/graph.ts
@@ -7,10 +7,11 @@ import {
   LegendComponent,
   type LegendComponentOption,
   ToolboxComponent,
+  GraphicComponent,
 } from 'echarts/components'
 import { GraphChart, type GraphSeriesOption } from 'echarts/charts'
 import { CanvasRenderer } from 'echarts/renderers'
-import data from './graph.json'
+import data from './data.json'
 
 echarts.use([
   TitleComponent,
@@ -19,6 +20,7 @@ echarts.use([
   GraphChart,
   CanvasRenderer,
   ToolboxComponent,
+  GraphicComponent,
 ])
 
 interface GraphNode {
@@ -38,70 +40,20 @@ type EChartsOption = echarts.ComposeOption<
   | GraphSeriesOption
 >
 
-const organizations: GraphNode[] = [
-  {
-    id: '0',
-    name: '农业农村部',
-    symbolSize: 20,
-    category: 0,
-  },
-  {
-    id: '1',
-    name: '生态环境部',
-    symbolSize: 13,
-    category: 1,
-  },
-  {
-    id: '2',
-    name: '国家发展与改革委员会',
-    symbolSize: 10,
-    category: 2,
-  },
-  {
-    id: '3',
-    name: '国家能源局',
-    symbolSize: 2,
-    category: 3,
-  },
-]
-
-interface GraphLink {
-  source: number
-  target: number
-  lineStyle: {
-    width: number
-  }
-}
-
-const links: GraphLink[] = [
-  {
-    source: 0,
-    target: 1,
-    lineStyle: {
-      width: 8,
-    },
-  },
-  {
-    source: 1,
-    target: 2,
-    lineStyle: {
-      width: 3,
-    },
-  },
-  {
-    source: 2,
-    target: 3,
-    lineStyle: {
-      width: 2,
-    },
-  },
-]
-
 interface Category {
   name?: string
 }
 
-const categories: Category[] = organizations.map(({ name }) => ({ name }))
+const central = data['中央']
+
+const categories: Category[] = []
+
+central.nodes.forEach((n) => {
+  n.x = Math.random() * 1000
+  n.y = Math.random() * 1000
+
+  categories.push({ name: n.name })
+})
 
 const title = '机关间联合发文关系'
 
@@ -112,7 +64,7 @@ export const graph = (element: HTMLDivElement, clear = false): void => {
 
   chart.hideLoading()
 
-  data.nodes.forEach((element: GraphNode) => {
+  central.nodes.forEach((element: GraphNode) => {
     element.label = {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       show: element.symbolSize! > 30,
@@ -122,6 +74,8 @@ export const graph = (element: HTMLDivElement, clear = false): void => {
   const option: EChartsOption = {
     title: {
       text: title,
+      top: 'bottom',
+      left: 'right',
     },
     toolbox: {
       show: true,
@@ -146,16 +100,16 @@ export const graph = (element: HTMLDivElement, clear = false): void => {
       {
         name: title,
         type: 'graph',
-        layout: 'force',
+        layout: 'none',
         force: {
-          repulsion: 100,
-          edgeLength: [30, 120],
+          repulsion: 400,
+          edgeLength: [800, 300],
         },
-        data: organizations,
-        links: links,
+        data: central.nodes,
+        links: central.links,
         categories: categories,
         roam: true,
-        draggable: true,
+        draggable: false,
         label: {
           position: 'right',
           formatter: '{b}',
@@ -175,4 +129,68 @@ export const graph = (element: HTMLDivElement, clear = false): void => {
   }
 
   chart.setOption(option)
+
+  initInvisibleGraphic()
+
+  function initInvisibleGraphic() {
+    // Add shadow circles (which is not visible) to enable drag.
+    chart.setOption({
+      graphic: echarts.util.map(
+        option.series[0].data,
+        function (item, dataIndex) {
+          //使用图形元素组件在节点上划出一个隐形的图形覆盖住节点
+          const tmpPos = chart.convertToPixel({ seriesIndex: 0 }, [
+            item.x,
+            item.y,
+          ])
+
+          return {
+            type: 'circle',
+            id: dataIndex,
+            position: tmpPos,
+            shape: {
+              cx: 0,
+              cy: 0,
+              r: 20,
+            },
+            // silent:true,
+            invisible: true,
+            draggable: true,
+            ondrag: echarts.util.curry(onPointDragging, dataIndex),
+            z: 100, //使图层在最高层
+          }
+        }
+      ),
+    })
+    window.addEventListener('resize', updatePosition)
+    chart.on('dataZoom', updatePosition)
+  }
+  chart.on('graphRoam', updatePosition)
+  function updatePosition() {
+    //更新节点定位的函数
+    chart.setOption({
+      graphic: echarts.util.map(
+        option.series[0].data,
+        function (item, dataIndex) {
+          const tmpPos = chart.convertToPixel({ seriesIndex: 0 }, [
+            item.x,
+            item.y,
+          ])
+
+          return {
+            position: tmpPos,
+          }
+        }
+      ),
+    })
+  }
+  function onPointDragging(dataIndex) {
+    //节点上图层拖拽执行的函数
+    const tmpPos = chart.convertFromPixel({ seriesIndex: 0 }, this.position)
+
+    option.series[0].data[dataIndex].x = tmpPos[0]
+    option.series[0].data[dataIndex].y = tmpPos[1]
+    chart.setOption(option)
+    updatePosition()
+  }
 }
